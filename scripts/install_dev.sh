30230718d26c5488c1efcff09ab1fa1643924af4
gnuhub <gnuhub@gmail.com>
2023-07-22 07:37:55 +0800

    Update install_dev.sh
    调试

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
+set -x
 # LANG=en_US.UTF-8
 is64bit=`getconf LONG_BIT`
 
